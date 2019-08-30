e2b1b6f62b47869aec344e1acc46e11d832ced82
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-30 19:35:23 +0200

    '' to ""

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 chars = string.ascii_letters + string.digits + "!@#$%^&*()"
 random.seed = (os.urandom(1024))
 
-url = '<request-receiver>'
+attackURL = "<receiver-url>"
 
 dummyData = json.loads(open('dummyData.json').read())
 
