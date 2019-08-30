8181d961d438ccdb12be77731bbf72dbaaecdc5d
Krampus Nuggets <sp3cter.productions@wemakeart.co.za>
2019-08-30 19:35:06 +0200

    '' to ""

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import string
 import json
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+chars = string.ascii_letters + string.digits + "!@#$%^&*()"
 random.seed = (os.urandom(1024))
 
 url = '<request-receiver>'
